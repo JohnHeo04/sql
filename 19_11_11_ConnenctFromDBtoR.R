20985c03b7beb8251bfd81fdcabb40009f470bd8
JohnHeo04 <47841046+JohnHeo04@users.noreply.github.com>
2019-11-11 11:30:10 +0900

    Add files via upload

--- 19_11_11_ConnenctFromDBtoR.R
+++ 19_11_11_ConnenctFromDBtoR.R
@@ -18,33 +18,50 @@ con <- dbConnect(
 
 dbSendQuery(con, "SET NAMES euckr")
 
+##### 연결해서 쿼리 작성하고 보내고 받고 굉장히 중요함.
 query.str <- "select * from as_test"
 query.rs <- dbSendQuery(con, query.str)
-rs <- fetch(query.rs)
+rs <- fetch(query.rs,-1)
 head(rs)
+dim(rs)
+
+class(rs)
+
+rs$접수방법 
+tb <- table(rs$접수방법)
+barplot(tb)
+
+
+
+##### 반대로 데이터를 DB로 입력하기
+
+iris
+dbWriteTable(con, "iris", iris, row.names=F)
+query.str <- "ALTER TABLE `test`.`iris`
+      CHANGE `Species` `Species` VARCHAR(20), 
+      CHANGE `Sepal.Length` `Sepal.Length` float" 
+
+dbRemoveTable(con, "iris")
+x
 
 
 
 
+query.rs <- dbSendQuery(con, query.str)
 
+dbRemoveTable(con, "iris")
 
+data <- read.csv(choose.files())
+dim(data)
+head(data)
+dbWriteTable(con, "export", data)
 
+dbListTables(con)
 
+dbReadTable(con, "미국맥주")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# DB강제로 끊기
+dbDisconnect(con)
 
 
 
